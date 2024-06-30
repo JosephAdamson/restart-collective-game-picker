bf12fb9a587b2b50c61a89813dde0084cd5abdc0
Joseph Adamson <josephadamson@Josephs-MacBook-Pro.local>
2024-06-30 20:13:26 +0100

    github pages deployment added

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/restart-collective-game-picker",
   server: {
     port: 8000
   }
